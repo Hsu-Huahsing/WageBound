01d06c43b2642215319390288f392b379c355411
Hsu-Huahsing <huahsing412@gmail.com>
2025-12-04 03:08:56 +0800

    1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='WageBound',  # 应用名
     author='Steven',
     license='BSD',
-    version='002',  # 版本号
+    version='0.0.3',  # 版本号
     packages=find_packages(),  # 包括在安装包内的Python包
     # include_package_data=True,  # 启用清单文件MANIFEST.in,包含数据文件
     exclude_package_data={'': ['.gitignore']},  # 上面的代码会将所有”.gitignore”文件排除在包外。如果上述exclude_package_date对象属性不为空，比如{'myapp':['.gitignore']}，就表明只排除”myapp”包下的所有”.gitignore”文件。
